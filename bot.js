833dcb57ae8104a2dfa8aa33a66ed0e4ef80175e
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-10-13 13:28:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = 'S'
 /////
-/////
+///// 
 /////
 const Util = require('discord.js');
 const getYoutubeID = require('get-youtube-id');
